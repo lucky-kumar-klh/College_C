a888796df7317d777bfc54cc1d1dd88cffab1bd3
Luckyx100 <technicallucky861@gmail.com>
2023-09-29 22:54:38 +0530

    commit

--- patterns/numberTriangle_palindrome.c
+++ patterns/numberTriangle_palindrome.c
@@ -6,17 +6,17 @@ int main ()
     for ( int i = 1; i <= n; i++ ) { 
       
         for (int m = 1; m <= n; m++){
-            if ( i+m < n+1 ) printf("  ");            
+            if ( i+m < n+1 ) printf("  ");     // print spaces 1st       
         }
        
         for ( int k = i; k >= 1; k-- ) { 
-            printf("%d ",k); 
+            printf("%d ",k);                   // print numbers in decreasing wise
         } 
 
-        printf(". ");
+        printf(". ");                          // print " . "
 
         for ( int j = 1; j <= i; j++ ) { 
-            printf("%d ",j); 
+            printf("%d ",j);                   // print no in increasing wise
         } 
 
         printf("\n"); 
