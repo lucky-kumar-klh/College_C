bf00de95b3eafb1bf96150f3d6f451407a3ad824
Luckyx100 <technicallucky861@gmail.com>
2023-10-01 20:19:53 +0530

    commit

--- Sample.c
+++ Sample.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
-#include <math.h>
-int main(){
-  int n,i;
-  scanf("%d",&n);
-  for(i=1;i<=n;i++)
-  {
-    int a=sqrt(i);
-    float b=sqrt(i);
-    if(a!=b) printf("%d ",i);
-  }
+#include<stdio.h> 
+int main(){ 
+    int n; 
+    float sum=0.0; 
+    scanf("%d",&n); 
+    printf("1/1"); 
+    for(float i=3;i<=n*2;){ 
+        printf(" + %f/%f",i,i-1); 
+        sum=sum+1.0+(i/(i-1)); 
+        i=i+2; 
+    } 
+    printf("\nSum of the series is %.3f",sum); 
 }
