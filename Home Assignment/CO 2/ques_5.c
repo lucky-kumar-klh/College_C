5ca74a24aae6c0db478a5e2430085c3747e77d03
Luckyx100 <technicallucky861@gmail.com>
2023-10-09 11:01:06 +0530

    commit

--- Home Assignment/CO 2/ques_5.c
+++ Home Assignment/CO 2/ques_5.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 int main(){
+    int n, k, j;
+    scanf("%d", &n);
+    int a = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        // even lines ( i = 1, 3, 5, ... )
+        if ( i%2 == 0 )
+        {
+            int b = n*i;
+            for ( j = n; j >= 1; j--)
+            {
+                printf("%d ", b--);
+            }
+        }
+        // odd lines ( i = 2, 4, 6, ... )
+        else {
+            for ( k = 1; k <= n; k++)
+            {
+                printf("%d ", a++);
+                if ( k == n ) a += n;
+            }
+        }
+        printf("\n");
+    }
     
     return 0;
 }
