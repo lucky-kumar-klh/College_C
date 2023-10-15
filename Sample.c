b478c9f86857c04a366f30975f5a41a5631887f8
Luckyx100 <technicallucky861@gmail.com>
2023-10-15 10:10:49 +0530

    commit

--- Sample.c
+++ Sample.c
@@ -1,44 +1,29 @@
 #include <stdio.h>
-#include <stdbool.h>
 int main(){
-    printf("Simple Calculator\n\n\n");
     int n;
-    float a, b;
-    bool x = 1;
-    while ( x ) {
-        printf("*********\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. Exit\n");
-        printf("\nEnter your choice : ");
-        scanf("%d", &n);
-        switch ( n ){
-        case 1:
-            printf("Enter 2 numbers for Addition : ");
-            scanf("%f %f", &a, &b);
-            printf("Result: %.2f + %.2f = %.2f\n\n", a, b, a+b);
-            break;
-        
-        case 2:
-            printf("Enter 2 numbers for Subtraction : ");
-            scanf("%f %f", &a, &b);
-            printf("Result: %.2f - %.2f = %.2f\n\n", a, b, a-b);
-            break;
-        
-        case 3:
-            printf("Enter 2 numbers for Multiplication : ");
-            scanf("%f %f", &a, &b);
-            printf("Result: %.2f * %.2f = %.2f\n\n", a, b, a*b);
-            break;
-        
-        case 4:
-            printf("Enter 2 numbers for Division : ");
-            scanf("%f %f", &a, &b);
-            if ( b == 0 ) printf("Error: Division by zero is not allowed.\n\n");
-            else printf("Result: %.2f / %.2f = %.2f\n\n", a, b, a/b);
-            break;
-        
-        case 5:
-            x = 0;
-            break;
+    scanf("%d", &n);
+    int a = (n/2+1);
+    int space = a, star = 0;
+    for ( int  i = 1; i <= n; i++)
+    {
+        if ( i <= a ){
+            space--;
+            star++;
         }
+        else {
+            space++;
+            star--;
+        }
+        
+        // spaces
+        for ( int j = 1; j <= space; j++){
+            printf("  ");
+        }
+        // stars
+        for ( int j = 1; j <= star; j++){
+            printf("+ ");
+        }
+        printf("\n");
     }
-    printf("Thank you for using the calculator.");
+    return 0;
 }
