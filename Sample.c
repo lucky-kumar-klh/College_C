1d838ab533f69765e879cf53ec94be8a59f7b231
Luckyx100 <technicallucky861@gmail.com>
2023-10-18 19:52:25 +0530

    commit

--- Sample.c
+++ Sample.c
@@ -2,10 +2,29 @@
 #include <math.h>
 #include <stdlib.h>
 #include <stdbool.h>
+bool isPrime(int a){
+    if ( a == 1 ) return 0;
+    for (int i = 2; i <= a/2; i++)
+    {
+        if ( a % i == 0 )  return 0;
+    }
+    return 1;
+}
+int sum(int b){
+    int sum = 0;
+    while ( b != 0 )
+    {
+        sum += b % 10;
+        b = b/10;
+    }
+    return sum;
+}
 int main(){
     int n;
     scanf("%d", &n);
-    float f = n*(1.8)+32.0;
-    printf("Temp is %.2f", f);
+    if ( isPrime(n) ){
+        printf("Sum is %d", sum(n));
+    }
+    else printf("No laddos");
     return 0;
 }
