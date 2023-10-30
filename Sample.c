490f900b578a6e0d5f650391275c358e8e73a781
Luckyx100 <technicallucky861@gmail.com>
2023-10-30 22:10:04 +0530

    commit

--- Sample.c
+++ Sample.c
@@ -2,14 +2,38 @@
 #include <math.h>
 #include <stdlib.h>
 #include <stdbool.h>
+int count_num(int n);
+int num(int x);
 int main(){
-    int num, digit;
-    scanf("%d", &num);
-    digit = (floor)(log10(num)+1);
-    if ( digit<= 5 )
-    {
-        printf("no of digits are %d", digit);
-    }
-    else printf("max 5 digit num only");
+    int n;
+    scanf("%d",&n);
+    printf("%d",count_num(n));
     return 0;
+}
+int count_num(int n)
+{
+  int k=n,count=0,x=num(n),y=num(n),fd,ld;
+  while(n!=0)
+  {
+    ld = n%10;
+    fd = n/pow(10,x-1);
+    if(k%ld==0 && k%fd==0) count+=2;
+    else if(k%ld==0 || k%fd==0) count++;
+    n = n - (fd)*pow(10,x-1);
+    n/=10;
+    x-=2;
+  }
+  if(k%2!=0 && count!=0) return count-1;
+  else return count;
+}
+
+int num(int x)
+{
+  int count=0;
+  while(x)
+  {
+    count++;
+    x/=10;
+  }
+  return count;
 }
