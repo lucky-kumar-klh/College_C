b2d7a6cdd5b03c2ac644d9bf451440bd3a7a926f
Luckyx100 <technicallucky861@gmail.com>
2023-11-02 13:54:23 +0530

    commit

--- ques.c
+++ ques.c
@@ -1,35 +1,35 @@
 #include <stdio.h>
 int main()
 {
-  int n,i,j,k=1;
-  scanf("%d",&n);
-  n/=2;
-  for(i=1;i<=n;i++,k++)
-  {
-    for(j=1;j<=n;j++)
+    int n,i,j,k=1;
+    scanf("%d",&n);
+    n/=2;
+    for(i=1;i<=n;i++,k++)
     {
-      if(j<=n-i+1) printf("%d ",k);
-      else printf("  ");
+      for(j=1;j<=n;j++)
+      {
+        if(j<=n-i+1) printf("%d ",k);
+        else printf("  ");
+      }
+      for(j=1;j<=n;j++)
+      {
+        if(j>=i) printf("%d ",k);
+        else printf("  ");
+      }
+      printf("\n");
     }
-    for(j=1;j<=n;j++)
+    for(i=1;i<=n;i++,k--)
     {
-      if(j>=i) printf("%d ",k);
-      else printf("  ");
+        for(j=1;j<=n;j++)
+        {
+            if(j<=i) printf("%d ",k);
+            else printf("  ");
+        }
+        for(j=1;j<=n;j++)
+        {
+          if(j>=n-i+1) printf("%d ",k);
+          else printf("  ");
+        }
+        printf("\n");
     }
-    printf("\n");
-  }
-  for(i=1;i<=n;i++,k--)
-  {
-    for(j=1;j<=n;j++)
-    {
-      if(j<=i) printf("%d ",k);
-      else printf("  ");
-    }
-    for(j=1;j<=n;j++)
-    {
-      if(j>=n-i+1) printf("%d ",k);
-      else printf("  ");
-    }
-    printf("\n");
-  }
 }
