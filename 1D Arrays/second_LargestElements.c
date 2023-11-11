7932b2f1b9fb0a9c9567459a74013160b462a4a8
Luckyx100 <technicallucky861@gmail.com>
2023-11-11 20:52:01 +0530

    commit

--- 1D Arrays/second_LargestElements.c
+++ 1D Arrays/second_LargestElements.c
@@ -14,13 +14,21 @@ int secondLargest(int arr[], int n){
 int secondSmallest(int arr[], int n){
     int min1 = INT_MAX, min2 = INT_MAX;
     // find smallest element 
-    
+    for (int i = 0; i < n; i++){
+        if ( arr[i] < min1 ) min1 = arr[i];
+    }
+    // find second smallest element
+    for (int i = 0; i < n; i++){
+        if ( arr[i] < min2 && arr[i] > min1 ) min2 = arr[i]; 
+    }
+    return min2;
 }
 int main(){
     int n;
     scanf("%d", &n);
     int arr[n];
     for (int i = 0; i < n; i++) scanf("%d", &arr[i]); 
-    printf("Second Largest Element = %d", secondLargest(arr, n));
+    printf("Second Largest Element = %d\n", secondLargest(arr, n));
+    printf("Second Smallest Element = %d", secondSmallest(arr, n));
     return 0;
 }
